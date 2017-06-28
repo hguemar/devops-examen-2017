5affa14e049a3a5d26a51e52a676fd44e77fb6e6
nosql <nosql@localhost.localdomain>
2017-06-28 19:56:55 +0200

    start from old tp

---
+++ test/testTask.js
@@ -0,0 +1,11 @@
+var assert = require("assert");
+var chai = require("chai");
+
+describe('Mocha', function () {
+
+    //result
+    it('test mocha', function () {
+        chai.assert.ok('ca fonctionne');
+    });
+
+});
